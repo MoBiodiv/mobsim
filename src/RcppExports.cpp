ebd2a66263a4f0079f5586d8e6d9406d3f319bed
Alban Sagouis <alban.sagouis@idiv.de>
2022-02-01 14:02:47 +0100

    reactivating albansagouis/mobsim fork

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // sSAC1_C
 NumericVector sSAC1_C(NumericVector x, NumericVector y, IntegerVector id_spec);
 RcppExport SEXP _mobsim_sSAC1_C(SEXP xSEXP, SEXP ySEXP, SEXP id_specSEXP) {
