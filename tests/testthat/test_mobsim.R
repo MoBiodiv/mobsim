e11c98d01ea93bf5ac97996351c26a22c44ea05a
Alban Sagouis <alban.sagouis@idiv.de>
2022-02-01 14:26:51 +0100

    update of tests for sim_thomas_coords()

--- tests/testthat/test_mobsim.R
+++ tests/testthat/test_mobsim.R
@@ -11,8 +11,6 @@ test_that("classes are correct", {
    expect_is(sim_thomas_coords(sad1, mother_points = 0), "community")
    expect_is(sim_thomas_coords(sad1, mother_points = 100), "community")
    expect_is(sim_thomas_coords(sad1, mother_points = 4.2), "community")
-   expect_is(sim_thomas_coords(sad1, mother_points = 1, cluster_points = 2), "community")
-   expect_is(sim_thomas_coords(sad1, mother_points = 1, xmother = xmother, ymother = ymother), "community")
    expect_is(sim_thomas_coords(sad1, xmother = xmother, ymother = ymother), "community")
    expect_is(sim_thomas_coords(sad1), "community")
    expect_is(sim_thomas_community(100, 1000), "community")
@@ -20,14 +18,14 @@ test_that("classes are correct", {
    expect_is(community_to_sad(sim_poisson_community(100,1000)), "sad")
 })
 
-test_that("The function handles wrong mother_points parametres", {
+test_that("The function sim_thomas_coords handles wrong mother_points parametres", {
    expect_error(sim_thomas_coords(sad1, mother_points = -1))  # mother_points is negative
    expect_error(sim_thomas_coords(sad1, mother_points = sample(size = 100, -2:2, replace = TRUE)))  # mother_points countains negative values
    expect_error(sim_thomas_coords(sad1, mother_points = rep(1, 4)))  # mother_points is too short
    expect_error(sim_thomas_coords(sad1, mother_points = rep(1, 1000)))  # mother_points is too long
 })
 
-test_that("The function handles wrong xmother and ymother parametres", {
+test_that("The function sim_thomas_coords handles wrong xmother and ymother parametres", {
    sad2 <- sim_sad(s_pool = 2, n_sim = 100)
    xmother <- lapply(1:length(sad2), function(x) runif(2, 0, 1))
 
@@ -36,13 +34,7 @@ test_that("The function handles wrong xmother and ymother parametres", {
    expect_error(sim_thomas_coords(sad2, xmother = xmother, ymother = list(2, c(0.2, 0.3))))
 })
 
-test_that("The function rThomas_rcpp behaves as expected", {
-   expect_is(rThomas_rcpp(20, n_mother_points = 1, xmother = .5, ymother = .5, sigma = 0.2), "data.frame")
-   expect_equal(nrow(rThomas_rcpp(20, n_mother_points = 1, xmother = .5, ymother = .5, sigma = 0.2)), 20)
-   expect_error(rThomas_rcpp(20, n_mother_points = 2, sigma = 0.2))
-})
-
-test_that("The function handles wrong xrange and yrange parametres", {
+test_that("The function sim_thomas_coords handles wrong xrange and yrange parametres", {
    expect_error(sim_thomas_coords(sad2, xmother = xmother, ymother = xmother, xrange = c(0, 0.1)))   # xmother and ymother outside of range
    expect_error(sim_thomas_coords(sad2, xmother = xmother, ymother = xmother, xrange = data.frame(c(0,1), c(0,0.1))))   # xrange and y range have different class
    expect_error(sim_thomas_coords(sad2, xmother = xmother, ymother = xmother,
@@ -53,8 +45,21 @@ test_that("The function handles wrong xrange and yrange parametres", {
                                   yrange = data.frame(c(0,1), c(0,1), c(0,1))))  # xrange and yrange have too many rows
 })
 
+test_that("The function sim_thomas_coords throws warnings when expected", {
+  sad1 <- sim_sad(s_pool = 100, n_sim = 1000)
+  xmother <- lapply(1:length(sad1), function(x) runif(1, 0, 1))
+  ymother <- lapply(1:length(sad1), function(x) runif(1, 0, 1))
+  expect_warning(sim_thomas_coords(sad1, mother_points = 1, xmother = xmother, ymother = ymother))
+  expect_warning(sim_thomas_coords(sad1, mother_points = 1, cluster_points = 2))
+})
 
-test_that("species richness and abundance are correct", {
+test_that("The function rThomas_rcpp behaves as expected", {
+   expect_is(rThomas_rcpp(20, n_mother_points = 1, xmother = .5, ymother = .5, sigma = 0.2), "data.frame")
+   expect_equal(nrow(rThomas_rcpp(20, n_mother_points = 1, xmother = .5, ymother = .5, sigma = 0.2)), 20)
+   expect_error(rThomas_rcpp(20, n_mother_points = 2, sigma = 0.2))
+})
+
+test_that("species richness and abundance are correct in sim_sad() and sim_thomas_community()", {
    sad1 <- sim_sad(100,1000, fix_s_sim = T)
    expect_equal(length(sad1), 100)
    expect_equal(sum(sad1), 1000)
