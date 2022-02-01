e743b844dcae6064fe1cd07cdfb015cf092dace7
Alban Sagouis <alban.sagouis@idiv.de>
2022-02-01 14:04:03 +0100

    update testthat.r for R CMD CHECK

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,6 +1,6 @@
 library(testthat)
 library(mobsim)
 
-#test_check("mobsim")
-#test_package("mobsim", reporter="summary")
-test_dir("./tests/testthat", reporter="summary")
+test_check("mobsim")
+# test_package("mobsim", reporter="summary")
+# test_dir("./tests/testthat", reporter="summary")
