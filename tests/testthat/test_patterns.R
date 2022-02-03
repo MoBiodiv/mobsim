2e9e6ff0d99e1ed0cf1b65c9167879ed02ce3990
Alban Sagouis <alban.sagouis@idiv.de>
2022-02-03 10:43:24 +0100

    Update test_patterns.R

--- tests/testthat/test_patterns.R
+++ tests/testthat/test_patterns.R
@@ -27,15 +27,42 @@ test_that("rare_curve() - handles 0 and NA values as expected", {
    expect_vector(rare_curve(sad0), size = 50L)
 })
 
+
+test_that("div_rect() & div_rand_rect() - return expected value classes and length", {
+   S = 5L
+   comm1 <- sim_thomas_coords(sim_sad(S, 50L))
+   div1 <- div_rect(x0 = 0, y0 = 0, xsize = 1, ysize = 1, comm = comm1)
+   divRand <- div_rand_rect(prop_area = 1, n_rect = 2, exclude_zeros = TRUE, comm = comm1)
+
+   expect_vector(div1, ptype = numeric(), size = 6)
+   expect_vector(divRand, ptype = numeric(), size = 12)
+})
+
 test_that("div_rect() - produces results comparable to vegan", {
    S = 5L
    comm1 <- sim_thomas_coords(sim_sad(S, 50L))
    div1 <- div_rect(x0 = 0, y0 = 0, xsize = 1, ysize = 1, comm = comm1)
 
-   expect_equal(as.integer(div1[1]), S)
-   expect_equal(as.integer(div1[2]), S)
+   expect_equal(as.numeric(div1[1]), S)
+   expect_equal(as.numeric(div1[2]), S)
    expect_equal(as.numeric(div1[3]), vegan::diversity(table(comm1$census$species), "shannon"))
    expect_equal(as.numeric(div1[4]), exp(vegan::diversity(table(comm1$census$species), "shannon")))
    expect_equal(as.numeric(div1[5]), vegan::diversity(table(comm1$census$species), "simpson"))
    expect_equal(as.numeric(div1[6]), 1 / (1 - vegan::diversity(table(comm1$census$species), "simpson")))
 })
+
+
+test_that("div_rand_rect() - produces results comparable to vegan", {
+   S = 5L
+   comm1 <- sim_thomas_coords(sim_sad(S, 50L))
+   divRand <- div_rand_rect(prop_area = 1, n_rect = 2, exclude_zeros = TRUE, comm = comm1)
+
+   expect_equal(as.numeric(divRand[1]), S)
+   expect_equal(as.numeric(divRand[3]), S)
+   expect_equal(as.numeric(divRand[5]), vegan::diversity(table(comm1$census$species), "shannon"))
+   expect_equal(as.numeric(divRand[7]), exp(vegan::diversity(table(comm1$census$species), "shannon")))
+   expect_equal(as.numeric(divRand[9]), vegan::diversity(table(comm1$census$species), "simpson"))
+   expect_equal(as.numeric(divRand[11]), 1 / (1 - vegan::diversity(table(comm1$census$species), "simpson")))
+})
+
+
