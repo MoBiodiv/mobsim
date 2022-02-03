02ab8a5ff075fbd4f437c6688b8133daa0a89282
Alban Sagouis <alban.sagouis@idiv.de>
2022-02-03 09:46:26 +0100

    adding tests for diversity metrics div_rect()

--- tests/testthat/test_patterns.R
+++ tests/testthat/test_patterns.R
@@ -26,3 +26,16 @@ test_that("rare_curve() - handles 0 and NA values as expected", {
    expect_error(rare_curve(sadNA))
    expect_vector(rare_curve(sad0), size = 50L)
 })
+
+test_that("div_rect() - produces results comparable to vegan", {
+   S = 5L
+   comm1 <- sim_thomas_coords(sim_sad(S, 50L))
+   div1 <- div_rect(x0 = 0, y0 = 0, xsize = 1, ysize = 1, comm = comm1)
+
+   expect_equal(as.integer(div1[1]), S)
+   expect_equal(as.integer(div1[2]), S)
+   expect_equal(as.numeric(div1[3]), vegan::diversity(table(comm1$census$species), "shannon"))
+   expect_equal(as.numeric(div1[4]), exp(vegan::diversity(table(comm1$census$species), "shannon")))
+   expect_equal(as.numeric(div1[5]), vegan::diversity(table(comm1$census$species), "simpson"))
+   expect_equal(as.numeric(div1[6]), 1 / (1 - vegan::diversity(table(comm1$census$species), "simpson")))
+})
